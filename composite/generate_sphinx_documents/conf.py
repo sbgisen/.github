bc6db211455a16412c71864aac4a788c873f2851
jsupratman13 <supratmanjoshua@gmail.com>
2023-04-12 12:32:54 +0900

    enable sphinx-multiversion

--- composite/generate_sphinx_documents/conf.py
+++ composite/generate_sphinx_documents/conf.py
@@ -45,11 +45,12 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'sphinx.ext.intersphinx',
-    'sphinx.ext.autodoc.typehints'
+    'sphinx.ext.autodoc.typehints',
+    'sphinx_multiversion'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ['../_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
